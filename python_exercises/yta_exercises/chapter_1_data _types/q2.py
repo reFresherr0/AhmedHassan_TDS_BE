8bbe116e811aaa0afa4a5c9e0243397c84fb9dc5
reFresherr0 <93086169+reFresherr0@users.noreply.github.com>
2023-10-31 09:40:43 +0200

    3 questions done.

---
+++ python_exercises/yta_exercises/chapter_1_data _types/q2.py
@@ -0,0 +1,6 @@
+num_1 = input("Enter first number:")
+num_2 = input("Enter second number:")
+num_3 = input("Enter third number:")
+
+avg = (float(num_1) + float(num_2) + float(num_3)) / 3
+print("Average of three numbers is: " + str(avg))
