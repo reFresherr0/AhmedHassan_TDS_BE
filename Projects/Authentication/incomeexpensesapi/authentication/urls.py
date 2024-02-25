e62a7f2195470fb84fcc9f689a73b67bfef54931
reFresherr0 <93086169+reFresherr0@users.noreply.github.com>
2024-02-25 21:15:08 +0200

    Delete Projects directory

--- Projects/Authentication/incomeexpensesapi/authentication/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import RegisterView, VerifyEmail, LoginAPIView
-
-
-urlpatterns = [
-    path('register/', RegisterView.as_view(), name='Register'),
-    path('login/', LoginAPIView.as_view(), name='login'),
-    path('email-verify/', VerifyEmail.as_view(), name='email-verify'),
-]
--- Projects/Drf_Socialapps/be_drf/socialapps/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SocialappsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'socialapps'
