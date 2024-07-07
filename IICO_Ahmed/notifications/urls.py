aa9a1dc990c83414ae5e9f7e489b7d4f62b1711a
reFresherr0 <93086169+reFresherr0@users.noreply.github.com>
2024-07-08 01:38:38 +0300

    Delete IICO_Ahmed directory

--- IICO_Ahmed/notifications/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',views.NotificationListCreateView.as_view(),name='Notifications-create'),
-    path('updateNotifications/<int:id>',views.NotificationRetrieveUpdateDestroyView.as_view(),name='Notifications-retrive-update-destroy'),
-    path('count',views.NotificationCountView.as_view(),name='Notifications-count'),
-    
-]
--- IICO_Ahmed/socialapps/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',views.SocialAppsViews.as_view(),name='Social-apps'),
-    path('update/<int:id>',views.SocialAppDetailView.as_view(),name='Social-apps'),
-]
